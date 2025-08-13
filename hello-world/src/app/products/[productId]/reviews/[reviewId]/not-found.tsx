0d741d7f5f342e529902eb7b28dbca2a4cdc8839
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-08-13 17:47:48 +0200

    not found page

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { usePathname } from "next/navigation";
+
 export default function NotFound() {
+  const pathname = usePathname();
+  const productId = pathname.split("/")[2];
+  const reviewId = pathname.split("/")[4];
+
   return (
     <div>
-      <h1>Review not found</h1>
+      <h1>
+        Review {reviewId} not found {productId}
+      </h1>
     </div>
   );
 }
