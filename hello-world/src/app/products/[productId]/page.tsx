9701146b0fb5c4f2a7c8f7eb81954d80edf8a167
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-08-15 10:12:11 +0200

    title Metadata

--- hello-world/src/app/products/[productId]/page.tsx
+++ hello-world/src/app/products/[productId]/page.tsx
@@ -1,3 +1,25 @@
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${id}`);
+    }, 1000);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
 export default async function Product({
   params,
 }: {
