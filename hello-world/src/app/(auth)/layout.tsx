7b3c361c68b887aea312be741ca384b92b89c72a
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-08-15 10:42:17 +0200

    active links

---
+++ hello-world/src/app/(auth)/layout.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
+
+const navlinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  return (
+    <div>
+      {navlinks.map((link) => {
+        const isActive =
+          pathname === link.href ||
+          (pathname.startsWith(link.href) && link.href !== "/");
+        return (
+          <Link
+            className={
+              isActive ? "font-bold text-xl mr-4" : "text-blue-500 mr-4"
+            }
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+}
--- hello-world/src/app/products/page.tsx
+++ hello-world/src/app/products/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
 export default function ProductList() {
+  const productId = 100;
+
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Products List!</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href={`/products/${productId}`} replace>
+          Product {productId}
+        </Link>
+      </h2>
     </>
   );
 }
