102af23825b1d5958e6341e07a955df592cba0fc
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-08-15 11:50:54 +0200

    navigating programmatically

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function Review({
   params,
@@ -8,7 +8,7 @@ export default async function Review({
   const { productId, reviewId } = await params;
 
   if (parseInt(reviewId) > 1000) {
-    notFound();
+    redirect('/products');
   }
 
   return (
