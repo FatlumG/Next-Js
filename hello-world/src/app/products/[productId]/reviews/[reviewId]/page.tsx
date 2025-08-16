f46f61795bdfd3cfb45fd436bf6fcf4387ed257d
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-08-16 10:50:54 +0200

    error handling

--- hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,14 +1,22 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function Review({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
-  const { productId, reviewId } = await params;
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review...");
+  }
 
+  const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
-    redirect('/products');
+    redirect("/products");
   }
 
   return (
