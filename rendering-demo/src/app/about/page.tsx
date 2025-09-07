a7b1865b944d2aa77892dd272f76e541e152f608
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-09-07 08:57:46 +0200

    generateStaticParams

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,6 +1,11 @@
-export default function AboutPage() {
-  const time = new Date().toLocaleTimeString();
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme, "theme");
+
   console.log("About server component");
 
-  return <h1>About Page {time}</h1>;
+  return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
